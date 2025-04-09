be647fc9f8b74a4477eddfc942322db253b01d7b
ÖMER MEVLÜTOĞLU <omaraliyt23@gmail.com>
2025-04-10 02:15:30 +0300

    Folder Structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxt/icon'
-  ]
-})
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/test-utils",
+    "@nuxt/icon",
+    "@nuxtjs/i18n",
+  ],
+  i18n: {
+    locales: ["en", "ar"],
+    defaultLocale: "en",
+  },
+  
+});
